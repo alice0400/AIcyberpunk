ffe1eb0c60b796d07ac148a2c3533b39e399067f
alice0400 <114126043+alice0400@users.noreply.github.com>
2025-12-13 01:41:05 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    nodePolyfills({
+      // บังคับให้ Polyfill เฉพาะ Buffer ที่ TON ต้องการ
+      include: ['buffer'],
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
+  base: '/',
+});
